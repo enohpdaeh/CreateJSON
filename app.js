ed8a65fc71325fd345df29abb11818ef75eb70c5
enohpdaeh <enohpdaeh@gmail.com>
2014-11-16 23:40:43 +0900

    lindaからデータ取得

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var LindaClient = require('linda').Client;
+var socket = require('socket.io-client').connect('http://linda-server.herokuapp.com/');
+var linda = new LindaClient().connect(socket);
+
+var ts = linda.tuplespace('delta');
+var name = "";
+var temp_value = "";
+var light_value = "";
+var http = require('http'), fs = require('fs');
+var now = new Date();
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -14,15 +25,6 @@ var YQL = require('yql');
 var query = new YQL('SELECT * FROM weather.bylocation WHERE location="Fujisawa" AND unit="c"');
 var weatherRepo;
 
-//fujisawaの天気を取得してjson化
-query.exec(function(err, data) {
-  var location = data.query.results.weather.rss.channel.location;
-  var condition = data.query.results.weather.rss.channel.item.condition;
-  //console.log("{weather :" + JSON.stringify(location) + "," + JSON.stringify(condition) + "}");
-  weatherRepo = "{\"weather\" :[" + JSON.stringify(location) + "," + JSON.stringify(condition) + "]}";
-  //weatherRepo = "[" + JSON.stringify(location) + "," + JSON.stringify(condition) + "]";
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -38,11 +40,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
-// /JSONにGETアクセスしたとき、JSONを返す
-app.get('/JSON', function(req, res) {
-  res.send(weatherRepo);
+//Lindaから明るさ、温度を取得
+linda.io.on('connect', function(){
+  print('socket.io connect!!');
+
+  ts.read({type:"sensor", name:"light"}, function(err, tuple){
+    if(err) return;
+    name = tuple.data.name;
+    light_value = tuple.data.value;
+  });
+  ts.read({type:"sensor", name:"temperature"}, function(err, tuple){
+    if(err) return;
+    name = tuple.data.name;
+    temp_value = tuple.data.value;
+  });
+
 });
 
+var print = function(msg){
+  console.log(msg);
+  if(typeof msg === 'object') msg = JSON.stringify(msg);
+};
+
+var http = require('http'), fs = require('fs');
+var now = new Date();
+
+//fujisawaの天気を取得してjson化
+function getRec(){
+  query.exec(function(err, data) {
+    var location = data.query.results.weather.rss.channel.location;
+    var condition = data.query.results.weather.rss.channel.item.condition;
+    var temp_light =  ',{"light":"' + light_value + '","temp":"' + temp_value + '","time":"' + now + '"}';
+    weatherRepo = "{\"weather\" :[" + JSON.stringify(location) + "," + JSON.stringify(condition) + temp_light + "]}";
+    console.log(weatherRepo);
+  });
+}
+
+// /JSONにGETアクセスしたとき、JSONを返す
+app.get('/JSON', function(req, res) {
+  getRec();
+  res.send(weatherRepo);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
